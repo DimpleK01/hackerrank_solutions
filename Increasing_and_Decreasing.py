2fc4c776cac695f4f2685df16f1163de56bcd998
dimple_k <68893917+DimpleK01@users.noreply.github.com>
2022-06-11 13:34:27 +0530

    Update Increasing_and_Decreasing.py

--- Increasing_and_Decreasing.py
+++ Increasing_and_Decreasing.py
@@ -1,8 +1,3 @@
-n1,n2=[int(x) for x in input().split()]
-
-
-arr1 = list(map(int,input().strip().split()))[:n1]
-arr2 = list(map(int,input().strip().split()))[:n2]
 
 arr1.sort()
 arr2.sort(reverse=True)
@@ -11,3 +6,11 @@ arr3=arr1+arr2
 
 for i in range(len(arr3)):
     print(arr3[i], end=" ")
+
+#Driver code
+
+n1,n2=[int(x) for x in input().split()]
+
+
+arr1 = list(map(int,input().strip().split()))[:n1]
+arr2 = list(map(int,input().strip().split()))[:n2]
