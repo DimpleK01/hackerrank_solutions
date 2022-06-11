43ec9389fd85ab6599da368b271d8095524d4b6b
dimple_k <68893917+DimpleK01@users.noreply.github.com>
2022-06-11 13:28:27 +0530

    Create Least_Occurring_element.py

---
+++ Least_Occurring_element.py
@@ -0,0 +1,17 @@
+n= int(input())
+arr= list(map(int, input().split()))
+
+duplicates = dict((x,arr.count(x)) for x in set(arr))
+
+duplicates = dict(sorted(duplicates.items(), key=lambda item: item[1]))
+values=list(duplicates.values())
+n = values.count(values[0]);
+li=[]
+keys=list(duplicates.keys())
+for i in range(n):
+    li.append(keys[i])
+li.sort(reverse=True) 
+for i in range(len(li)):
+    print (li[i], end=" ")
+    
+    
